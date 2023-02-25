af56ba54f6c221953b791f6229de373c632ffcfb
thiagohenrique7 <thiagohenriquean@gmail.com>
2023-02-25 16:12:56 -0300

    first

---
+++ build/classes/view/LoginView.rs
@@ -0,0 +1,3 @@
+app.view.LoginView$1
+app.view.LoginView$2
+app.view.LoginView
---
+++ build/classes/view/PainelPrincipalView.rs
@@ -0,0 +1,9 @@
+app.view.PainelPrincipalView$1
+app.view.PainelPrincipalView$8
+app.view.PainelPrincipalView$6
+app.view.PainelPrincipalView$7
+app.view.PainelPrincipalView$4
+app.view.PainelPrincipalView$5
+app.view.PainelPrincipalView$2
+app.view.PainelPrincipalView$3
+app.view.PainelPrincipalView
